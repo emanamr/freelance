798be0f66d46be0196008d8098d7f256d49d8dbc
eman amr <eng_eman5_9@hotmail.com>
2020-10-14 22:44:46 +0200

    second page

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
     });
 })*/
 
-
+//for open side menu
 function openNav() {
     document.getElementById("side").style.width = "250px";
     
@@ -22,3 +22,19 @@ function closeNav() {
     document.getElementById("side").style.width = "0px";
     
 }
+
+
+
+//to remove the place holder from the input when focuse
+$(function () {
+    'use strict';
+    //hide placeholder on form focus
+
+    $('[placeholder]').focus(function () {
+        $(this).attr('data-text', $(this).attr('placeholder'));
+        $(this).attr('placeholder', '');
+    }).blur(function () {
+        $(this).attr('placeholder', $(this).attr('data-text'));
+
+    });
+});
